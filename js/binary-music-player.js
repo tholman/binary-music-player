21bba1b2b64c0a6db7868fa629fa478497829f5b
Tim <timothy.w.holman@gmail.com>
2016-11-29 18:32:44 -0500

    scaffold

---
+++ js/binary-music-player.js
@@ -0,0 +1,10 @@
+/*!
+ * binary-music-player
+ *
+ * MIT licensed
+ * Copyright (C) 2013 Tim Holman, http://tholman.com
+ */
+
+/*********************************************
+ *
+ *********************************************/

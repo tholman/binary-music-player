f4c0e9f475849966b84f35344c7a5f110912e6b6
Tim <timothy.w.holman@gmail.com>
2017-01-17 11:49:15 -0500

    movement and ui scaffolding

---
+++ js/ui.js
@@ -0,0 +1,20 @@
+/**
+ * Binary Music Player UI
+ * - Handles user inputs, mostly
+ */
+
+// DOM
+var customizeButton;
+
+// VARS
+var isOpen = false;
+
+
+function bindUI() {
+
+  customizeButton = document.querySelector('info h1');
+
+
+}
+
+bindUI();

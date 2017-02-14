2a9dce14eccc2ce17b0dddbc6854893e1cae17d2
Tim <timothy.w.holman@gmail.com>
2017-02-13 23:48:44 -0500

    interactions work

--- js/ui.js
+++ js/ui.js
@@ -4,17 +4,45 @@
  */
 
 // DOM
-var customizeButton;
+var customizeButton, customizeArea, inputAreas, keys;
 
 // VARS
-var isOpen = false;
+var isFocused = 0;
 
+
+function cacheDOM() {
+  customizeButton = document.querySelector('.info');
+  customizeArea = document.querySelector('.editor-container');
+  inputAreas = document.querySelectorAll('.notes input');
+  keys = document.querySelectorAll('.piano-key');
+}
 
 function bindUI() {
+  
+  customizeButton.addEventListener('click', onCustomizeButtonClicked, false);
+  
+  for( var i = 0; i < keys.length; i++ ) {
+    keys[i].addEventListener('click', onKeyClicked, false);
+  }
+}
 
-  customizeButton = document.querySelector('info h1');
+function onCustomizeButtonClicked() {
 
+  customizeArea.classList.toggle('open');
+  inputAreas[0].focus();
+  isFocused = 0;
 
 }
 
+function onKeyClicked() {
+
+  isFocused = isFocused + 1;
+  if( isFocused >= inputAreas.length ) {
+    isFocused = 0;
+  }
+  
+  inputAreas[isFocused].focus();
+}
+
+cacheDOM();
 bindUI();
